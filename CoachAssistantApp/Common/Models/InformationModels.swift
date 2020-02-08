7581e4b07c0685ebc47320f84fbb19746e188031
Dimitar Parapanov <d.parapanov@abv.bg>
2020-02-08 16:39:22 +0200

    Initialize Model and Networking files

---
+++ CoachAssistantApp/Common/Models/InformationModels.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  CoachAssistantApp
+//
+//  Created by Yalishanda on 8.02.20.
+//  Copyright © 2020 Swift FMI. All rights reserved.
+//
+
+import Foundation
---
+++ CoachAssistantApp/Common/Models/Networking.swift
@@ -0,0 +1,9 @@
+//
+//  Networking.swift
+//  CoachAssistantApp
+//
+//  Created by Yalishanda on 8.02.20.
+//  Copyright © 2020 Swift FMI. All rights reserved.
+//
+
+import Foundation
