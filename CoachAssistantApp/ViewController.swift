fe8381f0b88e78cb756d05a6953fca6124391396
Julieta Ignatova <julietaignatova77@gmail.com>
2020-02-08 14:02:58 +0200

    Create empty XCode project

---
+++ CoachAssistantApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoachAssistantApp
+//
+//  Created by Julieta Ignatova on 2/8/20.
+//  Copyright © 2020 Swift FMI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
